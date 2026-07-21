f31b51e02820b3df8f3b62c56313da82e361780a
Cole Chrzan <cole@schematichq.com>
2026-07-21 16:59:29 -0400

    fix up pricing table

--- react/components/pricing-table.tsx
+++ react/components/pricing-table.tsx
@@ -1,32 +1,25 @@
 import { useEffect, useMemo, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import {
+  formatCurrency,
+  getPlanPrice,
+} from "@schematichq/schematic-components";
 
 import {
   ComponentspublicApi,
   Configuration,
-  type PlanViewPublicResponseData,
   type PublicPlansResponseData,
 } from "../components/api/componentspublic";
 import { PricingTable } from "./headless/pricing-table";
 
-const getPrice = (plan: PlanViewPublicResponseData, period?: string) => {
-  switch (period) {
-    case "year":
-      return plan.yearlyPrice;
-    case "quarter":
-      return plan.quarterlyPrice;
-    case "month":
-      return plan.monthlyPrice;
-  }
-};
-
 export function PricingTableElement() {
-  const [selectedPeriod, setSelectedPeriod] = useState("month");
-  const [selectedCurrency, setSelectedCurrency] = useState("month");
-  const [catalog, setCatalog] = useState<PublicPlansResponseData>();
+  const periods = [
+    { label: "Billed monthly", value: "month" },
+    { label: "Billed yearly", value: "year" },
+  ];
 
-  const periods = ["month", "year"];
-  const currencies = ["usd"];
+  const [selectedPeriod, setSelectedPeriod] = useState("month");
+  const [catalog, setCatalog] = useState<PublicPlansResponseData>();
 
   const sessionId = useMemo(() => uuidv4(), []);
   const api = useMemo(
@@ -52,69 +45,65 @@ export function PricingTableElement() {
 
   return (
     <PricingTable.Root
-      periods={periods}
-      currencies={currencies}
+      className="pt-16"
       onPeriodChange={(period) => {
         setSelectedPeriod(period);
       }}
-      onCurrencyChange={(currency) => {
-        setSelectedCurrency(currency);
-      }}
     >
-      {currencies.length > 1 && (
-        <PricingTable.CurrencyToggle>
-          {currencies.map((currency) => (
-            <PricingTable.CurrencyOption key={currency} value={currency}>
-              {currency.toUpperCase()}
-            </PricingTable.CurrencyOption>
-          ))}
-        </PricingTable.CurrencyToggle>
-      )}
-
-      {periods.length > 1 && (
-        <PricingTable.PeriodToggle>
-          {periods.map((period) => (
-            <PricingTable.PeriodOption key={period} value={period}>
-              {period}
-            </PricingTable.PeriodOption>
-          ))}
-        </PricingTable.PeriodToggle>
-      )}
-
-      <PricingTable.Label>Plans</PricingTable.Label>
-      <PricingTable.Section>
-        {catalog?.activePlans.map((plan) => (
-          <PricingTable.Card key={plan.id}>
-            <PricingTable.Name>{plan.name}</PricingTable.Name>
-            <PricingTable.Description>
-              {plan.description}
-            </PricingTable.Description>
-            <PricingTable.Price>
-              {getPrice(plan, selectedPeriod)?.price}
-            </PricingTable.Price>
-
-            <PricingTable.Entitlements>
-              {plan.entitlements?.map((entitlement) => (
-                <PricingTable.Entitlement key={entitlement.id}>
-                  {entitlement.feature?.name}
-                </PricingTable.Entitlement>
-              ))}
-            </PricingTable.Entitlements>
-
-            <PricingTable.Footer>
-              <PricingTable.CallToAction>Choose plan</PricingTable.CallToAction>
-            </PricingTable.Footer>
-          </PricingTable.Card>
+      <PricingTable.PeriodToggle className="flex self-center rounded-full border border-border float-end mb-4">
+        {periods.map((period) => (
+          <PricingTable.PeriodOption
+            key={period.value}
+            value={period.value}
+            className="flex grow basis-1/2 font-medium text-white px-6 py-3 text-nowrap leading-none rounded-full transition-all data-selected:font-semibold data-selected:bg-border"
+          >
+            {period.label}
+          </PricingTable.PeriodOption>
         ))}
-      </PricingTable.Section>
+      </PricingTable.PeriodToggle>
 
-      <PricingTable.Label>Add-ons</PricingTable.Label>
-      <PricingTable.Section>
-        {catalog?.activeAddOns.map((addOn) => (
-          <PricingTable.Card key={addOn.id}>
-            <PricingTable.Name>{addOn.name}</PricingTable.Name>
-          </PricingTable.Card>
-        ))}
+      <PricingTable.Label className="text-2xl font-semibold text-white mb-4">
+        Plans
+      </PricingTable.Label>
+
+      <PricingTable.Section className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4 flex-wrap clear-right">
+        {catalog?.activePlans.map((plan, idx) => {
+          const planPrice = getPlanPrice(plan, selectedPeriod)?.price;
+
+          return (
+            <PricingTable.Card
+              key={plan.id}
+              className="relative flex flex-col text-white border border-border bg-card rounded-xl p-6 shadow-2xl outline-4 outline-transparent data-active:outline-accent"
+            >
+              <PricingTable.Name className="text-4xl font-bold leading-none tracking-tight">
+                {plan.name}
+              </PricingTable.Name>
+              <PricingTable.Description className="text-muted-foreground text-lg mb-4">
+                {plan.description}
+              </PricingTable.Description>
+
+              {typeof planPrice === "number" && (
+                <PricingTable.Price className="text-3xl font-semibold tracking-tight mb-8">
+                  {formatCurrency(planPrice)}
+                </PricingTable.Price>
+              )}
+
+              <PricingTable.Entitlements className="grow list-disc pl-4 space-y-1 mb-8">
+                {plan.entitlements?.map((entitlement) => (
+                  <PricingTable.Entitlement key={entitlement.id}>
+                    {entitlement.feature?.name}
+                  </PricingTable.Entitlement>
+                ))}
+              </PricingTable.Entitlements>
+
+              <PricingTable.Footer>
+                <PricingTable.CallToAction className="flex justify-center w-full p-4 text-lg font-medium leading-none text-white bg-accent border border-accent rounded-lg transition-all">
+                  Choose plan
+                </PricingTable.CallToAction>
+              </PricingTable.Footer>
+            </PricingTable.Card>
+          );
+        })}
       </PricingTable.Section>
     </PricingTable.Root>
   );
